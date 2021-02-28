6c611874e6d66b1653ee7adbc1d50b94e977dea4
anjuhypp <anjuhypp@gmail.com>
2021-02-28 16:00:21 -0500

    die populate need to fix sum

--- dice.js
+++ dice.js
@@ -9,15 +9,23 @@ const rollDice = () => {
     let sum = 0;
     const numInput = Number(numOfDice.value);
     for (let i = 0; i < numInput; i++) {
-        let randomIdx = Math.floor(Math.random() * dice.length)
-        sum += randomIdx
+        let randomRoll = Math.floor(Math.random() * dice.length)
+        diceStr += dice[randomRoll];
+        // sum += randomRoll
+        // diceArray.push
     }
-    diceSum.innerText = `Sum = ${sum}`
+    diceSum.innerText = `Sum = ${diceStr} ${sum}`
+
+   
     // displayDice.innerText = dice[randomIdx]
     const li = document.createElement("li");
-    li.textContent = `${sum}`
+    li.textContent = `=${sum}`
     diceHistory.appendChild(li);
 };    
+
+for (const key of dice) {
+    console.log(key)
+}
 
 debugger
 
--- diceArray.js
+++ diceArray.js
@@ -1,12 +1,1 @@
-const dice = [
-    {1: "&#9856"},
-    {2: "&#9857"},
-    {3: "&#9858"},
-    {4: "&#9859"},
-    {4: "&#9860"},
-    {6: "&#9861"},
-];
-
-console.log(dice)
-console.log(Object.values(dice))
-console.log(Object.keys(dice))
+const dice = ["⚀","⚁","⚂","⚃","⚄","⚅"]
