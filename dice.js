993e9f2ea9f7e04335a2cb72d39ccba09587152f
anjuhypp <anjuhypp@gmail.com>
2021-02-28 18:06:27 -0500

    done

--- dice.js
+++ dice.js
@@ -1,8 +1,8 @@
 let numOfDice = document.querySelector("#number-of-dice");
 let diceButton = document.querySelector("#roll-the-dice-btn");
-let displayDice = document.querySelector("#display-dice");
 let diceSum = document.querySelector("#sum-para");
-let diceHistory = document.querySelector("#dice-para");
+let displayDice = document.querySelector("#dice-para");
+const ul = document.querySelector("ul")
 
 const rollDice = () => {
     let diceStr = "";
@@ -13,14 +13,12 @@ const rollDice = () => {
     diceStr += dice[randomIdx];
     sum += randomIdx + 1
     }
-    diceSum.innerText = `Sum = ${diceStr} ${sum}`;
+    diceSum.textContent = `Sum = ${diceStr} ${sum}`;
 
   const li = document.createElement("li");
 //   li.textContent = `${diceStr} = ${sum}`;
   li.textContent = diceStr + "=" + sum;
-  diceHistory.appendChild(li);
+  ul.appendChild(li);
 };
-
-debugger;
 
 diceButton.addEventListener("click", rollDice);
